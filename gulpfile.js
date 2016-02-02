b0b41223ea4a1e8579e480d63e8ff878c7945d74
Paul Kujawa <p.kujawa@gmx.net>
2016-02-02 22:55:40 +0100

    refs #5 markup recipe pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ gulp.task('default', function () {});
 var sass = sass = require('gulp-sass');
 gulp.task('sass', function () {
     gulp.src('./web/scss/*.scss')
-        .pipe(sass({sourceComments: 'map'}))
+        .pipe(sass({sourceComments: 'map', noCache: true}))
         .pipe(gulp.dest('./web/css/'));
 });
