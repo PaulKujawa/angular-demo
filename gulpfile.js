7ad428212286f78816867d3b4b3efd2e07c320b2
Paul Kujawa <p.kujawa@gmx.net>
2016-02-06 22:27:08 +0100

    refs #7 gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
-gulp.task('default', function () {});
+var sass = require('gulp-sass');
+var source = 'app/Resources/public/scss/**/*.scss';
+var output = 'web/css/';
 
-var sass = sass = require('gulp-sass');
 gulp.task('sass', function () {
-    gulp.src('./web/scss/*.scss')
-        .pipe(sass({sourceComments: 'map', noCache: true}))
-        .pipe(gulp.dest('./web/css/'));
+    return gulp.src(source)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('default', function() {
+   gulp.watch(source, ['sass']);
 });
