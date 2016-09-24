1c2ea3d889d4c38f851cfa4e4bdca8f0a6913bac
Paul Kujawa <p.kujawa@gmx.net>
2016-09-24 02:36:33 +0200

    46 angular 2.0

--- app/Resources/public/ts/app/bootstrap-compiled.ts
+++ app/Resources/public/ts/app/bootstrap-compiled.ts
@@ -2,6 +2,6 @@ import './rxjs';
 
 import {platformBrowser} from "@angular/platform-browser";
 //noinspection TypeScriptCheckImport
-import {AppModuleNgFactory} from "./app.module.ngfactory";
+import {AppModuleNgFactory} from "../app-compiled/app/app.module.ngfactory";
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
