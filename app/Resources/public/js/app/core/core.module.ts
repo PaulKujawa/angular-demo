7ae43f2052a924f99891447b716184a9aeab2a45
Paul Kujawa <p.kujawa@rebuy.de>
2017-08-26 00:50:42 +0200

    httpClient: replace default-request-options with interceptor

--- app/Resources/public/js/app/core/core.module.ts
+++ app/Resources/public/js/app/core/core.module.ts
@@ -2,7 +2,6 @@ import {CommonModule} from '@angular/common';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RequestOptions} from '@angular/http';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
 import {AuthComponent} from './component/auth.component';
@@ -12,10 +11,10 @@ import {PageableFactory} from './factory/pageable.factory';
 import {AuthRepository} from './repository/auth.repository';
 import {AuthenticationGuard} from './service/auth-guard.service';
 import {AuthenticationService} from './service/authentication.service';
-import {DefaultRequestOptions} from './service/default-request-options.service';
 import {DeviceDetectService} from './service/device-detection.service';
 import {FlashMessageInterceptor} from './service/flash-message.interceptor';
 import {FlashMessageService} from './service/flash-message.service';
+import {HeadersInterceptor} from './service/headers.interceptor';
 import {InViewportService} from './service/in-viewport.service';
 import {RoutingService} from './service/routing.service';
 import {TranslationService} from './service/translation.service';
@@ -47,8 +46,8 @@ import {TranslationService} from './service/translation.service';
         PageableFactory,
         RoutingService,
         TranslationService,
+        {provide: HTTP_INTERCEPTORS, useClass: HeadersInterceptor, multi: true},
         {provide: HTTP_INTERCEPTORS, useClass: FlashMessageInterceptor, multi: true},
-        {provide: RequestOptions, useClass: DefaultRequestOptions},
         {provide: 'windowObject', useFactory: WindowFactory},
     ],
 })
