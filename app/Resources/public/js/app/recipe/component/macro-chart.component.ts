2dfcbc97245bfbf4fe9c6fc17cddef045aa8f9ac
Paul Kujawa <p.kujawa@rebuy.de>
2017-12-30 00:58:35 +0100

    material: provide custom material sass mixxin

--- app/Resources/public/js/app/recipe/component/macro-chart.component.ts
+++ app/Resources/public/js/app/recipe/component/macro-chart.component.ts
@@ -4,19 +4,16 @@ import {Macros} from '../model/macros';
 @Component({
     selector: 'macro-chart',
     template: `
-        <div class="app-macro-chart">
-            <div style="background-color: #ffd54f"
-                 [style.width.%]="getProportion(macros.carbs)">
+        <div class="app-recipe-list_macro">
+            <div [style.width.%]="getProportion(macros.carbs)">
                 {{ getProportion(macros.carbs) | number:'1.0-0' }}
             </div>
 
-            <div style="background-color: #7986cb"
-                 [style.width.%]="getProportion(macros.protein)">
+            <div [style.width.%]="getProportion(macros.protein)">
                 {{ getProportion(macros.protein) | number:'1.0-0' }}
             </div>
 
-            <div style="background-color: #e57373"
-                 [style.width.%]="getProportion(macros.fat)">
+            <div [style.width.%]="getProportion(macros.fat)">
                 {{ getProportion(macros.fat) | number:'1.0-0' }}
             </div>
         </div>
