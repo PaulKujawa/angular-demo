29a739dcf0d599eef49f6715806fafd57a3a0b6c
Paul Kujawa <p.kujawa@rebuy.de>
2018-03-04 09:09:21 +0100

    del-cms: remove route animations

--- app/Resources/public/js/app/product/component/product-detail.component.ts
+++ app/Resources/public/js/app/product/component/product-detail.component.ts
@@ -1,19 +1,16 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
-import {slideInDownAnimation} from '../../core/animations';
 import {Product} from '../model/product';
 import {ProductState} from '../service/product.state';
 
 @Component({
-    animations: [slideInDownAnimation],
     template: `
         <app-product-form [product]="product|async">
         </app-product-form>
     `,
 })
 export class ProductDetailComponent implements OnInit {
-    @HostBinding('@routeAnimation') public routeAnimation = true;
     @HostBinding('style.display') public display = 'block';
     @HostBinding('style.position') public position = 'absolute';
     public product: Observable<Product | undefined>;
