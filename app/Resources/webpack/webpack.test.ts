fad08088eecf9c706abbe3136fd3f51a3ab79526
Paul Kujawa <p.kujawa@rebuy.de>
2018-04-10 11:11:05 +0200

    e2e: karma for webpack4

--- app/Resources/webpack/webpack.test.ts
+++ app/Resources/webpack/webpack.test.ts
@@ -1,7 +1,9 @@
+import {AngularCompilerPlugin} from '@ngtools/webpack';
 import {Configuration} from 'webpack';
 import {WebpackConfigArgs} from './webpack-args';
 import {getCommonConfig} from './webpack.common';
 const merge = require('webpack-merge');
+const path = require('path');
 
 // Called by commonJs module karma.conf.js
 export const webpackConfig = (args: WebpackConfigArgs): Configuration => {
@@ -10,9 +12,10 @@ export const webpackConfig = (args: WebpackConfigArgs): Configuration => {
         module: {
             rules: [
                 {
-                    test: /\.ts$/,
+                    test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
                     loader: '@ngtools/webpack',
-                }, {
+                },
+                {
                     test: /\.scss$/,
                     loader: 'null-loader',
                 }, {
@@ -21,10 +24,12 @@ export const webpackConfig = (args: WebpackConfigArgs): Configuration => {
                 },
             ],
         },
-        plugins: [],
-        resolve: {
-            extensions: ['.ts', '.js'], // no .scss
-        },
+        plugins: [
+            new AngularCompilerPlugin({
+                tsConfigPath: path.resolve(__dirname, '../public/js/tsconfig.json'),
+                entryModule: path.resolve(__dirname, '../public/js/app/app.module#AppModule'),
+            }),
+        ],
     };
 
     return merge(getCommonConfig(args), testConfig);
